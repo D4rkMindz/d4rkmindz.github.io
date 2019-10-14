549c980e4c469250aad2bcc2ccac9fd1dbe4e0a6
Björn Pfoster <bjoern.pfoster@gmail.com>
2019-10-14 10:29:40 +0200

    Added SEO plugin

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,10 +1,15 @@
 module.exports = {
-    title: 'My Blog',
-    description: 'A Blog and Documentation for myself',
+    title: 'A Dev Journal',
+    description: 'A pair of notes of a developer',
     base: '/',
     themeConfig: {
         nextLinks: false,
         prevLinks: false,
+        author: {
+            name: 'Björn Pfoster',
+            twitter: '@DevDarker',
+        },
+        domain: 'blog.darker.dev',
         repo: 'd4rkmindz/d4rkmindz.github.io',
         repoLabel: 'Contribute!',
         editLinks: true,
@@ -16,19 +21,37 @@ module.exports = {
                 path: '/tutorials/',
                 sidebarDepth: 2,
                 children: [
-                  ['/tutorials/php/', 'PHP'],
-                  ['/tutorials/xampp/', 'XAMPP']
+                    ['/tutorials/php/', 'PHP'],
+                    ['/tutorials/xampp/', 'XAMPP']
                 ]
             },
             ['/summaries/', 'Summaries'],
         ],
     },
     plugins: [
-        [
-            '@vuepress/google-analytics',
-            {
-                'ga': 'UA-109045851-6',
-            }
-        ]
+        ['@vuepress/google-analytics', {
+            'ga': 'UA-109045851-6',
+        }],
+        // https://github.com/lorisleiva/vuepress-plugin-seo
+        ['seo', {
+            siteTitle: (_, $site) => $site.title,
+            title: $page => $page.title,
+            description: $page => $page.frontmatter.description,
+            author: (_, $site) => $site.themeConfig.author,
+            tags: $page => $page.frontmatter.tags,
+            twitterCard: $page => $page.title + ' - by @DevDarker',
+            type: $page => {
+                if ($page.regularPath.startsWith('/blog')){
+                    return 'article';
+                }
+
+                return 'website';
+            },
+            url: (_, $site, path) => ($site.themeConfig.domain || '') + path,
+            image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image),
+            publishedAt: $page => $page.frontmatter.date && new Date($page.frontmatter.date),
+            modifiedAt: $page => $page.lastUpdated && new Date($page.lastUpdated),
+
+        }],
     ]
 };
